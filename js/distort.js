13f40ae24ccab872e9e1b8f06d76b1125c733dd9
atomicguy <atom@Adams-iMac-5K.local>
2015-08-16 10:07:21 -0400

    moved captions under images, correct for aspect ratio

--- js/distort.js
+++ js/distort.js
@@ -1,42 +1,69 @@
 function barrelDistortion(pixels) {
-    var d = pixels.data;
+    "use strict";
     var width = pixels.width;
     var height = pixels.height;
     var xmid = width / 2;
     var ymid = height / 2;
     var rMax = Math.sqrt(Math.pow(xmid, 2) + Math.pow(ymid, 2));
+    var x;
+    var y;
+    var gnRadius;
+    var x2;
+    var y2;
+    var gnRadius2;
+    var i;
+    var r;
+    var g;
+    var b;
+    var a;
+    var pr;
+    var alpha;
+    var sf;
+    var newR;
+    var newx;
+    var newy;
+    var tx;
+    var ty;
+    var newr;
 
-    var pix2D = new Array(pixels.height);
-    for (var y = 0; y < pixels.height; y++) {
-        pix2D[y] = new Array(pixels.width);
-        for (var x = 0; x < pixels.width; x++) {
-            var i = x * 4 + y * 4 * pixels.width;
-            var r = pixels.data[i],
-                g = pixels.data[i + 1],
-                b = pixels.data[i + 2],
-                a = pixels.data[i + 3];
 
-            var pr = Math.sqrt(Math.pow(xmid - x, 2) + Math.pow(ymid - y, 2)); //radius from pixel to pic mid
-            var sf = pr / rMax; //Scaling factor
-            var newR = pr * (0.19 * Math.pow(sf, 4) + 0.26 * Math.pow(sf, 2) + 1); //barrel distortion function
-            var alpha = Math.atan2(-(y - ymid), -(x - xmid)); //Get angle from pic mid to pixel vector
-            var newx = Math.abs(Math.cos(alpha) * newR - xmid); //get new x coord for this pixel
-            var newy = Math.abs(Math.sin(alpha) * newR - ymid); //get new y coord for this pixel
-            var gnRadius = Math.sqrt(Math.pow(xmid - newx, 2) + Math.pow(ymid - newy, 2)); //New radius (with new x - y values)
-            pix2D[y][x] = [r, g, b, a, newx, newy, newR, gnRadius]; //Make new y*x picture for reading pixels
+    var pix2D = [];
+    for (y = 0; y < pixels.height; y += 1) {
+        pix2D[y] = [];
+        for (x = 0; x < pixels.width; x += 1) {
+            i = x * 4 + y * 4 * pixels.width;
+            r = pixels.data[i];
+            g = pixels.data[i + 1];
+            b = pixels.data[i + 2];
+            a = pixels.data[i + 3];
+
+            // Find radius from pixel to mid point
+            pr = Math.sqrt(Math.pow(xmid - x, 2) + Math.pow(ymid - y, 2));
+            // Scale as needed
+            sf = pr / rMax;
+            // Barrel Distortion Function
+            newR = pr * (0.19 * Math.pow(sf, 4) + 0.26 * Math.pow(sf, 2) + 1);
+            // Get angle from mid point to pixel vector
+            alpha = Math.atan2(-(y - ymid), -(x - xmid));
+            // Get new X and Y Coordinants
+            newx = Math.abs(Math.cos(alpha) * newR - xmid);
+            newy = Math.abs(Math.sin(alpha) * newR - ymid);
+            // New radius (with new x & y values)
+            gnRadius = Math.sqrt(Math.pow(xmid - newx, 2) + Math.pow(ymid - newy, 2));
+            // Gather new pixel values
+            pix2D[y][x] = [r, g, b, a, newx, newy, newR, gnRadius];
         }
     }
 
     //Build new picture out of pix2D data
     var cnt = 0;
-    var inn = 0;
-    for (var y = 0; y < pix2D.length; y++) {
-        for (var x = 0; x < pix2D[y].length; x++) {
-            var tx = Math.round(pix2D[y][x][4]);
-            var ty = Math.round(pix2D[y][x][5]);
-            var newr = pix2D[y][x][6];
-            var gnRadius = pix2D[y][x][7];
-            if (Math.floor(newr) == Math.floor(gnRadius) && tx >= 0 && tx < width && ty >= 0 && ty < height) {
+    for (y2 = 0; y2 < pix2D.length; y2 += 1) {
+        for (x2 = 0; x2 < pix2D[y2].length; x2 += 1) {
+            tx = Math.round(pix2D[y2][x2][4]);
+            ty = Math.round(pix2D[y2][x2][5]);
+            newr = pix2D[y2][x2][6];
+            gnRadius2 = pix2D[y2][x2][7];
+            if (Math.floor(newr) === Math.floor(gnRadius2) && tx >= 0 && tx < width && ty >= 0 && ty < height) {
                 pixels.data[cnt++] = pix2D[ty][tx][0];
                 pixels.data[cnt++] = pix2D[ty][tx][1];
                 pixels.data[cnt++] = pix2D[ty][tx][2];
---
+++ js/stereoscope.js
@@ -0,0 +1,263 @@
+/*
+stereoscope.js
+
+Adam Schuster, 2015, askatom.net
+
+A tool to display vintage stereoviews on modern platforms
+
+The original stereoviews are loaded as JPS files and drawn on offscreen canvases.
+Canvas data is then Barrel Distorted to compensate for a VR viewer
+(specifially DODOcase / google cardboard and iPhone 6)
+Left and Right canvases are then drawn on a main stereovew canvas
+*/
+
+// Initilize size variables
+var wW = window.innerWidth;
+var wH = window.innerHeight;
+var cW = Math.round(wW * 0.5);
+var cH = Math.round(0.88 * wH);
+var canvasAspectRatio = cH / cW;
+// var ratioWH = cW / cH;
+
+// Initialize canvases
+var cL = document.createElement('canvas');
+var cR = document.createElement('canvas');
+var sV = document.getElementById('stereoview');
+
+// Initialize canvas contexts
+var cxL = cL.getContext('2d');
+var cxR = cR.getContext('2d');
+var cxV = sV.getContext('2d');
+
+// Initialize R & L card origin points
+var leftCardOriginX = 0;
+var leftCardOriginY = 0;
+var rightCardOriginX = cW;
+var rightCardOriginY = 0;
+
+// Get text objects from DOM
+var backgroundText = document.getElementById('pageInfo');
+var textLeft = document.getElementById('textLeft');
+var textRight = document.getElementById('textRight');
+var capStageBox = document.getElementById('captionBox');
+
+// Declare additional variables
+var curimg = 0;
+var card = new Image();
+var click;
+var caption = "";
+var newWidth = Number();
+var diff;
+var iW;
+var iH;
+var leftData;
+var rightData;
+
+// Calculate view screen dimensions
+
+function getWindowSize() {
+    "use strict";
+    wW = window.innerWidth;
+    wH = window.innerHeight;
+}
+
+// Initialize canvas elements
+cL.setAttribute('width', cW);
+cL.setAttribute('height', cH);
+
+cR.setAttribute('width', cW);
+cR.setAttribute('height', cH);
+
+// Set main stereoview canvas size
+function setSVSize() {
+    "use strict";
+    sV.setAttribute('width', wW);
+    sV.setAttribute('height', cH);
+}
+setSVSize();
+
+// Set canvas sizes
+
+function setCanvasSizes() {
+    "use strict";
+
+    cW = Math.round(wW * 0.5);
+    cH = Math.round(0.88 * wH);
+
+    sV.setAttribute('width', wW);
+    sV.setAttribute('height', cH);
+}
+
+function onDocumentMouseDown(event) {
+    "use strict";
+    event.preventDefault();
+
+    setCanvasSizes();
+
+    showCap();
+    blankCanvas();
+    shutterSound();
+    loadImage();
+
+    curimg = (curimg < galleryarray.length - 1) ? curimg + 1 : 0;
+}
+
+card.onload = function() {
+    "use strict";
+    drawCanvas();
+};
+
+function shutterSound() {
+    "use strict";
+    click = document.getElementById("click");
+    click.pause();
+    click.currentTime = 0;
+    click.play();
+}
+
+// Draw stereo content to canvases
+
+
+function drawCanvas() {
+    "use strict";
+
+    getImageSize();
+
+    if (card.height === 0) {
+
+        cxV.clearRect(0, 0, wW, wH);
+        sV.setAttribute('class', 'hiddenCanvas');
+
+    } else {
+
+        newWidth = Math.round(cH * iW / iH);
+        diff = Math.abs(Math.round((cW - newWidth) / 4));
+
+        cxL.clearRect(0, 0, newWidth, cH);
+        cxR.clearRect(0, 0, newWidth, cH);
+
+        cxL.drawImage(card, 0, 0, iW, iH, 0, 0, cW, cH);
+        cxR.drawImage(card, iW, 0, iW, iH, 0, 0, cW, cH);
+
+        leftData = cxL.getImageData(0, 0, cW, cH);
+        rightData = cxR.getImageData(0, 0, cW, cH);
+
+        leftData = barrelDistortion(leftData);
+        rightData = barrelDistortion(rightData);
+
+        cL.setAttribute('width', newWidth);
+        cR.setAttribute('width', newWidth);
+
+        cxL.putImageData(leftData, 0, 0);
+        cxR.putImageData(rightData, 0, 0);
+
+        leftCardOriginX = diff;
+        rightCardOriginX = cW + diff;
+
+        cxV.drawImage(cL, 0, 0);
+        cxV.drawImage(cR, cW, 0);
+
+        /*
+                cxV.putImageData(leftData, leftCardOriginX, leftCardOriginY);
+                cxV.putImageData(rightData, rightCardOriginX, rightCardOriginY);
+        */
+        sV.setAttribute('class', 'visibleCanvas');
+    }
+
+    /* debug
+        console.log("diff " + diff);
+        console.log("lx " + leftCardOriginX);
+        console.log("rx " + rightCardOriginX);
+        console.log("device pixel rato " + dpr);
+        console.log("card height " + cH);
+        console.log("card width " + cW);
+        console.log("window width " + wW);
+        console.log("image full width " + card.width);
+        console.log("image half width " + iW);
+        console.log("image height " + iH);
+        console.log("image ratio " + ratioWH);
+        console.log("new width " + newWidth);
+    */
+}
+
+// Get size of image
+
+
+function getImageSize() {
+    "use strict";
+
+    if (card.height === 0) {
+        iW = 1;
+        iH = 1;
+        ratioWH = 1;
+
+    } else {
+
+        iW = Math.round(card.width * 0.5);
+        iH = card.height;
+
+        var imageAspectRatio = iH / iW;
+        var cW2 = Math.round(iH / imageAspectRatio);
+        var cH2 = Math.round(iW * imageAspectRatio);
+
+        if (imageAspectRatio > canvasAspectRatio) {
+            cL.setAttribute('height', cH);
+            cL.setAttribute('width', cW2);
+            cR.setAttribute('height', cH);
+            cR.setAttribute('width', cW2);
+        } else {
+            cL.setAttribute('height', cH2);
+            cL.setAttribute('width', cW);
+            cR.setAttribute('height', cH2);
+            cR.setAttribute('width', cW);
+        }
+
+        leftData = cxL.createImageData(iW, iH);
+        rightData = cxR.createImageData(iW, iH);
+    }
+
+}
+
+// Blank canvas
+
+
+function blankCanvas() {
+    "use strict";
+    cxV.clearRect(0, 0, wW, wH);
+    sV.setAttribute('class', 'hiddenCanvas');
+    backgroundText.setAttribute('class', 'hiddenCanvas');
+}
+
+// reveal rendered stereo views
+
+
+function loadImage() {
+    "use strict";
+    card.src = "cards/" + galleryarray[curimg];
+}
+
+// Show captions
+
+
+function showCap() {
+    "use strict";
+    caption = textarray[curimg];
+    textLeft.innerHTML = caption;
+    textRight.innerHTML = caption;
+}
+
+
+document.addEventListener('mousedown', onDocumentMouseDown, false);
+document.addEventListener('touchstart', onDocumentMouseDown, false);
+
+// Handle window resizes
+
+function onWindowResize() {
+    "use strict";
+    getWindowSize();
+    setSVSize();
+    setCanvasSizes();
+    drawCanvas();
+}
+
+window.addEventListener('resize', onWindowResize, false);
